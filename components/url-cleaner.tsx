a7dffff01c5f9c77809ff1f50b61a4fe6e4be8c4
TUrielRios <riostiziano6@gmail.com>
2026-01-28 17:53:20 -0300

    [UPDATE] build no falla

--- components/url-cleaner.tsx
+++ components/url-cleaner.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 
-export function URLCleaner() {
+function URLCleanerContent() {
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
@@ -47,4 +47,12 @@ export function URLCleaner() {
     }, [searchParams, pathname]);
 
     return null;
+}
+
+export function URLCleaner() {
+    return (
+        <Suspense fallback={null}>
+            <URLCleanerContent />
+        </Suspense>
+    );
 }
